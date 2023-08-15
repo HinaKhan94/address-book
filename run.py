d8881290b7ea62cc2f21c2dfcebd3f3370854fcc
HinaKhan94 <hinakhan94.hk@gmail.com>
2023-08-15 21:02:20 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+
+    #creating an address list
+    addressList = []
+
+    choice = 0
+    while choice !=4:
+        print('** Address Manager **')
+        print('1) Add a contact')
+        print('2) Look up a contact')
+        print('3) Display all contacts')
+        print('4) Quit')
+        choice = int(input())
+
+        if choice == 1:
+            print('Adding a contact...')
+            nPerson = input("Enter the contact's full name:   ")
+            contact = input("Enter the contact number:   ")
+            address = input("Enter the address:   ")
+            addressList.append([nPerson, contact, address])
+
+        elif choice == 2:
+            print('Looking up for a contact...')    
+            term = input('Enter the name:  ')
+            for i in addressList:
+                if term in i:
+                    print(i)
+
+if __name__ == '__main__':
+    main()
