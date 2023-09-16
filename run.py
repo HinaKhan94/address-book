9b555de7deaa4863d8ae868945d2052eb06e8aea
HinaKhan94 <hinakhan94.hk@gmail.com>
2023-09-16 12:13:39 +0000

    chore: typos in readme.md deployment section

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('address_manager')
+SHEET = GSPREAD_CLIENT.open('Address Manager')
 
 
 class Color:
